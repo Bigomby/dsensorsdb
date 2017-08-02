8e95286a18c41342340c627eebc6262f907629d1
Diego Fernández Barrera <bigomby@gmail.com>
2017-08-02 16:39:56 +0200

    :sparkles: Update network module

--- src/network/bindings.rs
+++ src/network/bindings.rs
@@ -6,3 +6,11 @@ use libc::c_char;
 pub extern "C" fn network_new(address: NetAddress, name: *const c_char) -> *mut Network {
     unimplemented!()
 }
+
+#[no_mangle]
+pub extern "C" fn network_get_name(network_ptr: *const Network) -> *const c_char {
+    assert!(!network_ptr.is_null());
+    let network = unsafe { &*network_ptr };
+
+    network.get_name().as_ptr() as *const c_char
+}
